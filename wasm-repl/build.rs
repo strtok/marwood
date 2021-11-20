0e58ae4f11512de445ed694f4553d1c84b07e388
Erik Bremen <strtok@gmail.com>
2021-11-20 10:25:09 -0800

    Revert "add missing build.rs"
    This reverts commit 0fb608e877d92a7b933d9d819d975cfc4b84bff1.

--- wasm-repl/build.rs
+++
@@ -1,9 +0,0 @@
-use anyhow::Result;
-use vergen::{vergen, Config, ShaKind, TimestampKind};
-
-fn main() -> Result<()> {
-    let mut config = Config::default();
-    *config.git_mut().sha_kind_mut() = ShaKind::Short;
-    *config.build_mut().kind_mut() = TimestampKind::All;
-    vergen(config)
-}
