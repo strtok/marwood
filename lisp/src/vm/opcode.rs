235131e5d528a2be70f90ca0513c344568f26d73
Erik Bremen <strtok@gmail.com>
2021-12-31 17:09:02 -0800

    refactor decompiler

--- lisp/src/vm/opcode.rs
+++ lisp/src/vm/opcode.rs
@@ -1,7 +1,9 @@
 use crate::vm::vcell::VCell;
 use crate::vm::Vm;
+use lazy_static::lazy_static;
+use std::collections::HashMap;
 
-#[derive(Debug, Clone, Eq, PartialEq)]
+#[derive(Debug, Clone, Eq, Hash, PartialEq)]
 pub enum OpCode {
     Add,
     Car,
@@ -16,6 +18,57 @@ pub enum OpCode {
     Halt,
     Push,
     Sub,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+enum Operand {
+    Immediate,
+    Reference,
+    Dereference,
+}
+
+struct Schema {
+    name: &'static str,
+    operands: Vec<Operand>,
+}
+
+impl Schema {
+    fn new(name: &'static str, operands: Vec<Operand>) -> Schema {
+        Schema { name, operands }
+    }
+}
+
+fn schema() -> &'static HashMap<OpCode, Schema> {
+    lazy_static! {
+        static ref SCHEMA: HashMap<OpCode, Schema> = HashMap::from([
+            (OpCode::Add, Schema::new("ADD", vec![])),
+            (OpCode::Car, Schema::new("CAR", vec![])),
+            (OpCode::Cdr, Schema::new("CDR", vec![])),
+            (OpCode::Cons, Schema::new("CONS", vec![])),
+            (
+                OpCode::EnvGet,
+                Schema::new("ENVGET", vec![Operand::Reference])
+            ),
+            (
+                OpCode::EnvSet,
+                Schema::new("ENVSET", vec![Operand::Reference])
+            ),
+            (OpCode::Eq, Schema::new("EQ", vec![])),
+            (
+                OpCode::Mov,
+                Schema::new("MOV", vec![Operand::Dereference, Operand::Reference])
+            ),
+            (
+                OpCode::MovImmediate,
+                Schema::new("MOV", vec![Operand::Immediate, Operand::Reference])
+            ),
+            (OpCode::Mul, Schema::new("MUL", vec![])),
+            (OpCode::Halt, Schema::new("HALT", vec![])),
+            (OpCode::Push, Schema::new("PUSH", vec![])),
+            (OpCode::Sub, Schema::new("SUB", vec![]))
+        ]);
+    }
+    &SCHEMA
 }
 
 impl Vm {
@@ -40,49 +93,41 @@ impl Vm {
 
     pub fn decompile(&self, program: &[VCell]) -> Vec<(String, Vec<String>, Vec<String>)> {
         let mut cur = program.iter();
-        let mut result: Vec<(String, Vec<String>, Vec<String>)> = vec![];
-        while let Some(vcell) = cur.next() {
-            result.push(match vcell {
-                VCell::OpCode(ref op) => match op {
-                    OpCode::Add => ("ADD".into(), vec![], vec![]),
-                    OpCode::Car => ("CAR".into(), vec![], vec![]),
-                    OpCode::Cdr => ("CDR".into(), vec![], vec![]),
-                    OpCode::Cons => ("CONS".into(), vec![], vec![]),
-                    OpCode::EnvGet => {
-                        let arg = cur.next().unwrap();
-                        ("ENVGET".into(), vec![arg.to_string()], vec![])
+        let mut result = vec![];
+        while let Some(VCell::OpCode(opcode)) = cur.next() {
+            let schema = schema().get(opcode).expect("unknown opcode");
+            let mut operands = vec![];
+            let mut values = vec![];
+            for it in &schema.operands {
+                let operand = cur.next().expect("expected operand");
+                if operand.is_ptr() && values.is_empty() {
+                    values.push(self.heap.get_as_cell(operand).to_string());
+                }
+                match it {
+                    Operand::Immediate | Operand::Reference => {
+                        operands.push(operand.to_string());
                     }
-                    OpCode::EnvSet => {
-                        let arg = cur.next().unwrap();
-                        ("ENVSET".into(), vec![arg.to_string()], vec![])
+                    Operand::Dereference => {
+                        operands.push(format!("[{}]", operand));
                     }
-                    OpCode::Eq => ("EQ".into(), vec![], vec![]),
-                    OpCode::Halt => ("HALT".into(), vec![], vec![]),
-                    OpCode::Mov => {
-                        let src = cur.next().unwrap();
-                        let dest = cur.next().unwrap();
-                        (
-                            "MOV".into(),
-                            vec![format!("[{}]", src.to_string()), dest.to_string()],
-                            vec![],
-                        )
+                }
+
+                if values.is_empty() {
+                    match operand {
+                        VCell::Ptr(_) => {
+                            values.push(self.heap.get_as_cell(operand).to_string());
+                        }
+                        VCell::EnvSlot(slot) => {
+                            let ptr = self.globenv.get_slot(*slot);
+                            values.push(self.heap.get_as_cell(&ptr).to_string());
+                        }
+                        _ => {}
                     }
-                    OpCode::MovImmediate => {
-                        let src = cur.next().unwrap();
-                        let dest = cur.next().unwrap();
-                        (
-                            "MOV".into(),
-                            vec![src.to_string(), dest.to_string()],
-                            vec![],
-                        )
-                    }
-                    OpCode::Mul => ("MUL".into(), vec![], vec![]),
-                    OpCode::Push => ("PUSH".into(), vec![], vec![]),
-                    OpCode::Sub => ("SUB".into(), vec![], vec![]),
-                },
-                _ => ("UNKNOWN".into(), vec![], vec![]),
-            });
+                }
+            }
+            result.push((schema.name.to_string(), operands, values));
         }
+
         result
     }
 }
