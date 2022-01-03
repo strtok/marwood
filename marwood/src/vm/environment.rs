24a1390b9b1d8d2227e5b7b1d5a6a3cf5fff0e28
Erik Bremen <strtok@gmail.com>
2022-01-03 11:01:53 -0800

    breakup find_symbols

--- marwood/src/vm/environment.rs
+++ marwood/src/vm/environment.rs
@@ -4,134 +4,6 @@ use crate::vm::Error;
 use lazy_static::lazy_static;
 use std::collections::{HashMap, HashSet};
 
-/// Given a cell, recursively collect a list of free (unbound) symbols.
-///
-/// A symbol is 'free' if at the time it is encountered it meets the
-/// following conditions:
-///
-/// * It is a symbol
-/// * It does not yet exist in 'env'
-/// * It is not a primitive procedure (+, if, cons, etc.)
-/// * It is not currently being defined (e.g. first argument of a define procedure)
-///
-/// While recursing, any formal arguments to lambda procedures will be added
-/// to 'environment' and no longer represent free symbols.
-///
-/// # Arguments
-/// `cell` - The cell to recurse
-pub fn free_symbols(
-    cell: &Cell
-) -> Result<HashSet<&Cell>, Error> {
-
-    fn free_symbols<'a>(cell: &'a Cell,
-                        env: &mut HashSet<&'a Cell>,
-                        free: &mut HashSet<&'a Cell>) -> Result<(), Error>
-    {
-        match cell {
-            Cell::Symbol(_) => match env.contains(&cell) {
-                true => Ok(()),
-                false => {
-                    free.insert(cell);
-                    Ok(())
-                },
-            },
-            Cell::Pair(car, cdr) => {
-                let car = car.as_ref();
-                let cdr = cdr.as_ref();
-                if car.is_quote() {
-                    return Ok(());
-                }
-                if !car.is_primitive_symbol() && !env.contains(car) {
-                    free.insert(car);
-                }
-
-                let mut lat = match car {
-                    Cell::Symbol(sym) => match sym.as_str() {
-                        "define" => cdr
-                            .cdr()
-                            .ok_or_else(|| Error::InvalidNumArgs("define".into()))?,
-                        "lambda" => {
-                            let mut args = cdr
-                                .car()
-                                .ok_or_else(|| Error::InvalidNumArgs("lambda".into()))?;
-                            while args.is_pair() {
-                                let sym = args.car().unwrap();
-                                if !sym.is_symbol() {
-                                    return Err(Error::InvalidArgs(
-                                        "lambda".into(),
-                                        "argument".into(),
-                                        sym.to_string(),
-                                    ));
-                                }
-                                env.insert(sym);
-                                args = args.cdr().unwrap();
-                            }
-                            cdr.cdr()
-                                .ok_or_else(|| Error::InvalidNumArgs("lambda".into()))?
-                        }
-                        _ => cdr,
-                    },
-                    _ => cdr,
-                };
-
-                while lat.is_pair() {
-                    free_symbols(lat.car().unwrap(), env, free)?;
-                    lat = lat.cdr().unwrap();
-                }
-
-                // improper list
-                if !lat.is_nil() {
-                    free_symbols(lat, env, free)?;
-                }
-
-                Ok(())
-            }
-            _ => Ok(()),
-        }
-    }
-
-    let mut env = HashSet::new();
-    let mut free_set = HashSet::new();
-    free_symbols(cell, &mut env, &mut free_set)?;
-    Ok(free_set)
-}
-
-impl Cell {
-    pub fn is_quote(&self) -> bool {
-        self.is_symbol_str("quote")
-    }
-
-    pub fn is_lambda(&self) -> bool {
-        self.is_symbol_str("lambda")
-    }
-
-    pub fn is_symbol_str(&self, s: &'static str) -> bool {
-        match self.as_symbol() {
-            Some(sym) => sym == s,
-            _ => false,
-        }
-    }
-
-    /// Is Primitive Symbol
-    ///
-    /// Return true if the given cell is a primitive symbol (e.g. a built-in
-    /// procedure)
-    ///
-    /// # Arguments
-    /// `cell`
-    pub fn is_primitive_symbol(&self) -> bool {
-        lazy_static! {
-            static ref PRIMITIVE_SYMBOLS: HashSet<&'static str> = HashSet::from([
-                "car", "cdr", "cons", "define", "eq?", "eqv?", "lambda", "quote", "+", "-", "*"
-            ]);
-        }
-        match self {
-            Cell::Symbol(sym) => PRIMITIVE_SYMBOLS.contains(sym.as_str()),
-            _ => false,
-        }
-    }
-}
-
 /// Environment
 ///
 /// Environment represents a binding of a symbol to a value in the heap.
@@ -246,6 +118,150 @@ impl Environment {
 impl Default for Environment {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+/// Free Symbols
+///
+/// Given a cell, recursively collect a list of free (unbound) symbols.
+///
+/// A symbol is 'free' if at the time it is encountered it meets the
+/// following conditions:
+///
+/// * It is a symbol
+/// * It does not yet exist in 'env'
+/// * It is not a primitive procedure (+, if, cons, etc.)
+/// * It is not currently being defined (e.g. first argument of a define procedure)
+///
+/// While recursing, any formal arguments to lambda procedures will be added
+/// to 'environment' and no longer represent free symbols.
+///
+/// # Arguments
+/// `cell` - The cell to recurse
+pub fn free_symbols(
+    cell: &Cell
+) -> Result<HashSet<&Cell>, Error> {
+    let mut env = HashSet::new();
+    let mut free_set = HashSet::new();
+    find_free_symbols(cell, &mut env, &mut free_set)?;
+    Ok(free_set)
+}
+
+
+/// Find Free Symbols
+///
+/// This is a recursive call from free_syumbols.
+fn find_free_symbols<'a>(cell: &'a Cell,
+                         env: &mut HashSet<&'a Cell>,
+                         free: &mut HashSet<&'a Cell>) -> Result<(), Error>
+{
+    match cell {
+        Cell::Symbol(_) => match env.contains(&cell) {
+            true => Ok(()),
+            false => {
+                free.insert(cell);
+                Ok(())
+            },
+        },
+        Cell::Pair(car, cdr) => {
+            let car = car.as_ref();
+            let cdr = cdr.as_ref();
+            find_free_symbols_in_proc((car, cdr), env, free)?;
+            Ok(())
+        }
+        _ => Ok(()),
+    }
+}
+
+/// Find Free Symbols In Proc
+///
+/// This is a recursive call from free_syumbols.
+fn find_free_symbols_in_proc<'a>((car, cdr): (&'a Cell, &'a Cell),
+                                 env: &mut HashSet<&'a Cell>,
+                                 free: &mut HashSet<&'a Cell>) -> Result<(), Error>
+{
+    if car.is_quote() {
+        return Ok(());
+    }
+    if !car.is_primitive_symbol() && !env.contains(car) {
+        free.insert(car);
+    }
+
+    let mut lat = match car {
+        Cell::Symbol(sym) => match sym.as_str() {
+            "define" => cdr
+                .cdr()
+                .ok_or_else(|| Error::InvalidNumArgs("define".into()))?,
+            "lambda" => {
+                let mut args = cdr
+                    .car()
+                    .ok_or_else(|| Error::InvalidNumArgs("lambda".into()))?;
+                while args.is_pair() {
+                    let sym = args.car().unwrap();
+                    if !sym.is_symbol() {
+                        return Err(Error::InvalidArgs(
+                            "lambda".into(),
+                            "argument".into(),
+                            sym.to_string(),
+                        ));
+                    }
+                    env.insert(sym);
+                    args = args.cdr().unwrap();
+                }
+                cdr.cdr()
+                    .ok_or_else(|| Error::InvalidNumArgs("lambda".into()))?
+            }
+            _ => cdr,
+        },
+        _ => cdr,
+    };
+
+    while lat.is_pair() {
+        find_free_symbols(lat.car().unwrap(), env, free)?;
+        lat = lat.cdr().unwrap();
+    }
+
+    // improper list
+    if !lat.is_nil() {
+        find_free_symbols(lat, env, free)?;
+    }
+
+    Ok(())
+}
+
+impl Cell {
+    pub fn is_quote(&self) -> bool {
+        self.is_symbol_str("quote")
+    }
+
+    pub fn is_lambda(&self) -> bool {
+        self.is_symbol_str("lambda")
+    }
+
+    pub fn is_symbol_str(&self, s: &'static str) -> bool {
+        match self.as_symbol() {
+            Some(sym) => sym == s,
+            _ => false,
+        }
+    }
+
+    /// Is Primitive Symbol
+    ///
+    /// Return true if the given cell is a primitive symbol (e.g. a built-in
+    /// procedure)
+    ///
+    /// # Arguments
+    /// `cell`
+    pub fn is_primitive_symbol(&self) -> bool {
+        lazy_static! {
+            static ref PRIMITIVE_SYMBOLS: HashSet<&'static str> = HashSet::from([
+                "car", "cdr", "cons", "define", "eq?", "eqv?", "lambda", "quote", "+", "-", "*"
+            ]);
+        }
+        match self {
+            Cell::Symbol(sym) => PRIMITIVE_SYMBOLS.contains(sym.as_str()),
+            _ => false,
+        }
     }
 }
 
