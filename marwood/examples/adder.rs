8492167b0e2a010e66fe624945c32c4348207004
Erik Bremen <strtok@gmail.com>
2022-01-14 11:56:08 -0800

    add adder example

---
+++ marwood/examples/adder.rs
@@ -0,0 +1,25 @@
+use log::error;
+use marwood::cell::Cell;
+use marwood::vm::Vm;
+use marwood::{cell, lex, list, parse};
+
+extern crate marwood;
+
+fn main() {
+    let mut vm = Vm::new();
+
+    vm.eval(&parse!(
+        "(define make-adder (lambda (x) (lambda (y) (+ x y))))"
+    ))
+    .unwrap();
+
+    vm.eval(&parse!("(define add-1000 (make-adder 1000))"))
+        .unwrap();
+
+    for it in 0..10 {
+        match vm.eval(&list!["add-1000", it]) {
+            Ok(result) => assert_eq!(result, cell![1000 + it]),
+            Err(e) => error!("error: {}", e),
+        }
+    }
+}
