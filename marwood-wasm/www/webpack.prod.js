cec4aadb6502899d8753b13e56c25e67c1ddebfc
Erik Bremen <strtok@gmail.com>
2022-02-11 18:03:47 -0800

    build production mode

---
+++ marwood-wasm/www/webpack.prod.js
@@ -0,0 +1,25 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+  mode: "production",
+  plugins: [new CopyWebpackPlugin({
+    patterns: ["index.html"]
+  })],
+  experiments: {
+    asyncWebAssembly: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
