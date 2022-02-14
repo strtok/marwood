1bbe4d13fc28b744ac3a76b4f53b24eb309626d9
Erik Bremen <strtok@gmail.com>
2022-02-13 17:11:19 -0800

    require at lest 1 arg for display

--- marwood/src/vm/builtin/ports.rs
+++ marwood/src/vm/builtin/ports.rs
@@ -3,7 +3,7 @@ use crate::vm::vcell::VCell;
 use crate::vm::{Error, Vm};
 
 pub fn display(vm: &mut Vm) -> Result<VCell, Error> {
-    pop_argc(vm, 0, Some(1), "car")?;
+    pop_argc(vm, 1, Some(1), "display")?;
     let obj = vm.heap.get_as_cell(vm.stack.pop()?);
     vm.display(&obj);
     Ok(VCell::Void)
