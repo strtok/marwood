529deb74f712ba0a4d6f9439c6f33dbabe7e8cb8
Erik Bremen <strtok@gmail.com>
2022-02-18 16:06:43 -0800

    add call/cc tests

--- marwood/src/lib.rs
+++ marwood/src/lib.rs
@@ -1096,4 +1096,29 @@ mod integration_test {
             "x" => "45"
         ]
     }
+
+    #[test]
+    fn continuations() {
+        evals![
+            "(call/cc (lambda (cc) (cc 10)))" => "10"
+        ];
+
+        evals![
+            "(define animal (call/cc (lambda (cc) cc)))" => "#<void>",
+            "(animal 'elephant)" => "#<void>",
+            "animal" => "elephant"
+        ];
+
+        evals![
+            "(call-with-current-continuation (lambda (cc) (+ 2 5)))" => "7",
+            "(call-with-current-continuation (lambda (cc) (+ 2 5 (cc 3))))" => "3"
+        ];
+
+        evals![
+            "(define add100 0)" => "#<void>",
+            "(+ 100 (call/cc (lambda (cc) (set! add100 cc) 0)))" => "100",
+            "(add100 16)" => "116",
+            "(* 2 (add100 16))" => "116"
+        ];
+    }
 }
