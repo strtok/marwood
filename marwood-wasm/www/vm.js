419370e03b7c2526330dca942df7a6cf89e3d726
Erik Bremen <strtok@gmail.com>
2022-03-27 11:45:55 -0700

    yield less to the browser

--- marwood-wasm/www/vm.js
+++ marwood-wasm/www/vm.js
@@ -100,13 +100,13 @@ export class Vm {
 
     let result;
     if (this.evalIter == 0) {
-      result = this.marwood.eval(this.remainingInput, 10000);
+      result = this.marwood.eval(this.remainingInput, 250000);
       this.remainingInput = result.remaining;
       if (this.remainingInput != null && this.remainingInput.length == 0) {
         this.remainingInput = null;
       }
     } else {
-      result = this.marwood.eval_continue(25000);
+      result = this.marwood.eval_continue(250000);
     }
 
     if (result.completed) {
